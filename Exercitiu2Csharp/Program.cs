c5721988d861ba34093bc0d3e1c5cb2079517649
ionutl <ionut.lumina@gmail.com>
2015-05-12 17:11:58 +0300

    primele 2

---
+++ Exercitiu2Csharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiu2Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintInfoSeason(Season.Summer);
+        }
+
+        public enum Season
+        {
+            Spring = 1,
+            Summer = 2,
+            Autumn = 3,
+            Winter = 4
+        }
+
+        public static void PrintInfoSeason(Season season)
+        {
+            switch (season)
+            {
+                case Season.Spring:
+                    {
+                        Console.WriteLine("This season is {0}", season);
+                        break;
+                    }
+                case Season.Summer:
+                    {
+                        Console.WriteLine("Now it is {0}", season);
+                        break;
+                    }
+                case Season.Autumn:
+                    {
+                        Console.WriteLine("This is the {0} season", season);
+                        break;
+                    }
+                case Season.Winter:
+                    {
+                        Console.WriteLine("It is quite cold because it is {0}", season);
+                        break;
+                    }
+            }
+         }  
+    }
+}
---
+++ TestExercitii/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercitii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString(), Meth(DateTime.Now));
+        }
+
+        static bool Meth(DateTime date)
+        {
+            if ((date.DayOfWeek == DayOfWeek.Saturday) || (date.DayOfWeek == DayOfWeek.Sunday))
+            {
+                Console.WriteLine("weekend");
+                return true;
+            }
+            else
+                Console.WriteLine("is not weekend");
+                return false;
+        }
+    }
+}
