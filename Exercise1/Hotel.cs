eaa5d1fbb5def0619ca454688b3699efcf4cc0bc
ionutl <ionut.lumina@gmail.com>
2015-05-13 11:09:14 +0300

    2ndDay_Ex1

---
+++ Exercise1/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Hotel
+    {
+        public string name;
+        public string description;
+        public string address;
+        public int stars;
+        public double distanceToCenter;
+        DateTime openingDate;
+        public int[] rooms;
+    }
+}
---
+++ Exercise1/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Room
+    {
+        public string description;
+        public int places;
+        public int number;
+        public int floor;
+        public enum RoomTypes
+        {
+            Single,
+            Double,
+            Twin,
+            Duplex,
+            KingBedroom
+        };
+    }
+}
