972c101365987ec39f06eb20c6157c0a0a4710e5
ionutl <ionut.lumina@gmail.com>
2015-05-20 10:20:20 +0300

    ex13

--- FZTHMVC/DataAccess/Hotel.cs
+++ FZTHMVC/DataAccess/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,17 @@ namespace FZTHMVC.DataAccess
     public class Hotel
     {
         public Int32 Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public String Name { get; set; }
         public String Adress { get; set; }
         public String City { get; set; }
         public String Site { get; set; }
+        [Required]
+        [Range(1, 2000)]
         public Int32 Rooms { get; set; }
+        [Required]
+        [Range(1, 7)]
         public Int32 Rating { get; set; }
     }
 }
